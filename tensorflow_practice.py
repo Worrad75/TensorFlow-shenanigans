23a4f92070d9afaf63b21015e4f37d664ba0ac30
github username <github email address>
2020-07-19 22:40:26 -0400

    initial files

---
+++ tensorflow_practice.py
@@ -0,0 +1,7 @@
+import tensorflow
+from tensorflow import keras
+import numpy as np
+
+data = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
